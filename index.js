d663aaab51a97627fece402839d81175398947d5
K-Fort <fortink01@gmail.com>
2024-07-05 10:55:51 -0400

    practicing modularity with object oriented programming

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
+//const taskTitles = [];
+//const taskComplete = [];
+//const taskDescriptions = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
+// We will also add a description to the task
+//function newTask(title, description) {
+//  taskTitles.push(title);
+//  taskDescriptions.push(description);
+//  tasksComplete.push(false);
+//}
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
+/* function completeTask(taskIndex) {
   taskComplete[taskIndex] = true;
 }
 
@@ -28,4 +31,35 @@ newTask("Do Laundry"); // task 1
 
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+logTaskState(0); // Clean Cat Litter has been completed */
+
+//The above method is not modular and can become complicated.
+// Instead, we will use object oriented programming to increase modularity.
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+  };
+  return task;
+}
+
+// DRIVER CODE CHANGES BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
+
+
